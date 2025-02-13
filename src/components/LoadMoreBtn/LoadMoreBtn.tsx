7d872da898aa3671bed6e22057746e8ef3766ef4
Viktar Baloshka <vboloshko@gmail.com>
2025-02-13 22:01:34 +0100

    xifed

--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -1,12 +1,11 @@
 import clsx from "clsx";
 import s from "./LoadMoreBtn.module.css";
+import { LoadMoreBtnProps } from "./LoadMoreBtn.typr";
 
-export default function LoadMoreBtn({ handleChangePage }) {
+export default function LoadMoreBtn({ handleChangePage }: LoadMoreBtnProps) {
   return (
-    <>
-      <button onClick={handleChangePage} className={clsx(s.loadMoreBtn)}>
-        Load more
-      </button>
-    </>
+    <button onClick={handleChangePage} className={clsx(s.loadMoreBtn)}>
+      Load more
+    </button>
   );
 }
